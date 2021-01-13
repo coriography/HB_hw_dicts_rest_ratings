ab548e55e2197291f18fae60c3e3c0f96a4970d6
Cori Lint <corilint@gmail.com>
2021-01-12 21:32:41 -0600

    Added code for process_scores() function

--- ratings.py
+++ ratings.py
@@ -1,4 +1,24 @@
 """Restaurant rating lister."""
 
+def process_scores():
+    """Read scores file and return dictionary of {restaurant-name: score}."""
 
-# put your code here
+    scores_txt = open('scores.txt')
+
+    scores = {}
+
+    for line in scores_txt:
+        line = line.rstrip()
+        restaurant, score = line.split(":")
+        scores[restaurant] = int(score)
+    return scores
+
+
+
+# # sort dictionary by key
+# dict_ratings = sorted(scores)
+# # loop through keys again to print?
+# for rating in scores:
+#     # print variables in f-string
+#         # output example: Big Bean Shack is rated at 3.
+#     print(f"{rating} is rated at {rating}")
